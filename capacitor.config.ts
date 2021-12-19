1cbcb736dfeaac27263249e253812b39cdb1cd97
Saif Ali Khan <saffellikhan@gmail.com>
2021-12-20 00:29:39 +0500

    Capacitor Added.

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.oridune.app",
+  appName: "oridune",
+  webDir: "build",
+  bundledWebRuntime: false,
+};
+
+export default config;
