a93465542bb5063e53884d2bd004c4cd6496d521
Saif Ali Khan <68871807+abdollahkhan@users.noreply.github.com>
2022-01-04 02:51:01 +0500

    Minor Changes

--- src/App.routes.tsx
+++ src/App.routes.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Button } from "./components/core/button/button";
 import { ScrollToTop } from "./components/utils/scrollToTop";
 import { PageLayout } from "./layouts/page";
 import { ErrorPage } from "./pages/error";
@@ -16,37 +15,7 @@ export const AppRoutes: React.FC = () => {
           <Route path="/" element={<PageLayout />}>
             <Route
               path="/"
-              element={
-                <div className="w-100 h-screen p-20 bg-white dark:bg-black">
-                  <div className="grid grid-flow-row">
-                    <div className="col-span mb-10">
-                      <Button
-                        label={`Toggle Mode '${ToggleMode}'`}
-                        className="capitalize"
-                        onClick={() => {
-                          setToggleMode((prev) => {
-                            document.body.classList.remove(prev);
-                            if (prev === "dark") {
-                              document.body.classList.add("light");
-                              return "light";
-                            } else {
-                              document.body.classList.add("dark");
-                              return "dark";
-                            }
-                          });
-                        }}
-                      />
-                    </div>
-                    <div className="col-span">
-                      <Button
-                        onClick={(_, setBusy) => {
-                          setBusy((prev) => (prev = !prev));
-                        }}
-                      />
-                    </div>
-                  </div>
-                </div>
-              }
+              element={<h1>Hello World!</h1>}
             />
           </Route>
 
