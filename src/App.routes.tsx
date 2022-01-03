9a513c4021af5136bd31e02ad88ccae505c3375a
Saif Ali Khan <68871807+abdollahkhan@users.noreply.github.com>
2022-01-04 02:52:25 +0500

    Minor Changes

--- src/App.routes.tsx
+++ src/App.routes.tsx
@@ -1,22 +1,16 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ScrollToTop } from "./components/utils/scrollToTop";
 import { PageLayout } from "./layouts/page";
 import { ErrorPage } from "./pages/error";
 
 export const AppRoutes: React.FC = () => {
-  const [ToggleMode, setToggleMode] = useState<"dark" | "light">("light");
-
   return (
     <BrowserRouter>
       <ScrollToTop>
         <Routes>
           {/* Public routes includes Homepage, Contact Us, About Us etc. */}
           <Route path="/" element={<PageLayout />}>
-            <Route
-              path="/"
-              element={<h1>Hello World!</h1>}
-            />
+            <Route path="/" element={<h1>Hello World!</h1>} />
           </Route>
 
           {/* Not Found */}
