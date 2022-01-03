1cda44f7d5de8099427057c6a831c5e4246d2640
Saif Ali Khan <68871807+abdollahkhan@users.noreply.github.com>
2022-01-04 01:57:33 +0500

    Added a Button Component

--- src/App.routes.tsx
+++ src/App.routes.tsx
@@ -1,28 +1,59 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Button } from "./components/core/button/button";
 import { ScrollToTop } from "./components/utils/scrollToTop";
-import { Translate } from "./hooks/translator";
 import { PageLayout } from "./layouts/page";
 import { ErrorPage } from "./pages/error";
 
-export const AppRoutes: React.FC = () => (
-  <BrowserRouter>
-    <ScrollToTop>
-      <Routes>
-        {/* Public routes includes Homepage, Contact Us, About Us etc. */}
-        <Route path="/" element={<PageLayout />}>
-          <Route
-            path="/"
-            element={
-              <h1>
-                <Translate string={"Hello World!"} />
-              </h1>
-            }
-          />
-        </Route>
+export const AppRoutes: React.FC = () => {
+  const [ToggleMode, setToggleMode] = useState<"dark" | "light">("light");
 
-        {/* Not Found */}
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </ScrollToTop>
-  </BrowserRouter>
-);
+  return (
+    <BrowserRouter>
+      <ScrollToTop>
+        <Routes>
+          {/* Public routes includes Homepage, Contact Us, About Us etc. */}
+          <Route path="/" element={<PageLayout />}>
+            <Route
+              path="/"
+              element={
+                <div className="w-100 h-screen p-20 bg-white dark:bg-black">
+                  <div className="grid grid-flow-row">
+                    <div className="col-span mb-10">
+                      <Button
+                        label={`Toggle Mode '${ToggleMode}'`}
+                        className="capitalize"
+                        onClick={() => {
+                          setToggleMode((prev) => {
+                            document.body.classList.remove(prev);
+                            if (prev === "dark") {
+                              document.body.classList.add("light");
+                              return "light";
+                            } else {
+                              document.body.classList.add("dark");
+                              return "dark";
+                            }
+                          });
+                        }}
+                      />
+                    </div>
+                    <div className="col-span">
+                      <Button
+                        onClick={(_, setBusy) => {
+                          setBusy((prev) => (prev = !prev));
+                        }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              }
+            />
+          </Route>
+
+          {/* Not Found */}
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </ScrollToTop>
+    </BrowserRouter>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
+  mode: "jit",
+  darkMode: "class",
   content: ["./src/**/*.{html,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#E85D04",
+        secondary: "#FAA307",
+        tertiary: "#F48C06",
+      },
+    },
   },
   plugins: [],
 };
